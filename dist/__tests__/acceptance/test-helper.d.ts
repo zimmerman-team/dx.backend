7225e4ec27c975919e8393c2ab740282f858d3a7
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2023-05-10 11:24:20 +0200

    chore: update gitignore and remove dist folder

--- dist/__tests__/acceptance/test-helper.d.ts
+++
@@ -1,7 +0,0 @@
-import { DxBackendSolrApplication } from '../..';
-import { Client } from '@loopback/testlab';
-export declare function setupApplication(): Promise<AppWithClient>;
-export interface AppWithClient {
-    app: DxBackendSolrApplication;
-    client: Client;
-}
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationConfig, DxBackendSolrApplication } from './application';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<DxBackendSolrApplication>;
