4a7f263342a092b61fbec2e96a4dae37e928e8ac
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2023-06-07 10:42:26 +0200

    feat: add csv dataset preprocessing step adding fieldname prefix and cleaning dataset

--- src/controllers/dataset.controller.ts
+++ src/controllers/dataset.controller.ts
@@ -17,6 +17,11 @@ export class FileController {
       return { error: coreCreationStatus };
     }
 
+    const datasetPrepStatus = await _preprocessCSV(fileName);
+    if (datasetPrepStatus !== 'Success') {
+      return { error: datasetPrepStatus };
+    }
+
     const solrPostStatus = await _postDatasetToSolr(name, fileName);
     if (solrPostStatus !== 'Success') {
       return { error: solrPostStatus };
@@ -152,4 +157,61 @@ function _addSSRDataScraperEntry(name: any) {
   };
   additionalDatasets.push(dsObj);
   fs.writeFileSync(datascraperDatasetsLoc, JSON.stringify(additionalDatasets, null, 2));
+}
+
+const _preprocessCSV = async (fileName: string) => {
+  try {
+    const stagingDir = './staging/';
+    // the file is a CSV file. It is comma separated. I want to read it, and change any number that is not a decimal to end with .0
+    const fileContents = fs.readFileSync(stagingDir + fileName, 'utf8');
+    const lines = fileContents.split('\n');
+    const newLines = [];
+    let isHeader = true;
+    for (const line of lines) {
+      // if the line is a newline, skip it
+      if (line === '') continue;
+      let splitLine = line;
+      if (isHeader) splitLine = _removeInternalCommas(line);
+      const values = splitLine.split(',');
+      const newValues = [];
+      for (const value of values) {
+        // if (isHeader) value = replace value with just the letters and numbers in the name
+        if (isHeader) {
+          let newValue = value.replace(/[^a-zA-Z0-9]/g, '');
+          // prefix newValue with fileName
+          newValue = fileName.split('.')[0] + '__' + newValue;
+          newValues.push(newValue);
+        }
+        // first check if the value is a number
+        else if (!isNaN(Number(value))) {
+          const newValue = value.includes('.') ? value : value + '.0';
+          newValues.push(newValue);
+        } // else if the value is a string, check if it starts with an empty space and if so remove it.
+        else if (value.startsWith(' ')) {
+          newValues.push(value.substring(1));
+        } else {
+          newValues.push(value);
+        }
+      }
+      if (isHeader) isHeader = false;
+      newLines.push(newValues.join(','));
+    }
+    fs.writeFileSync(stagingDir + fileName, newLines.join('\n'));
+    return 'Success';
+  } catch (error) {
+    console.error(`Error preprocessing CSV: ${error}`);
+    return 'Failed to preprocess CSV';
+  }
+}
+
+const _removeInternalCommas = (line: string) => {
+  let insideQuotes = false;
+  let result = '';
+
+  for (const element of line) {
+    if (element === '"') insideQuotes = !insideQuotes;
+    if (element === ',' && insideQuotes) continue;
+    result += element;
+  }
+  return result
 }
