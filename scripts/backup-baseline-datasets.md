ef3734c5372a087d3b12d6700e11fa78139f62f8
Psami-wondah <okechukwusamuel16@gmail.com>
2024-04-04 14:48:19 +0100

    chore: create a readme file, format with black

---
+++ scripts/backup-baseline-datasets.md
@@ -0,0 +1,8 @@
+## This script backs up parsed json files of public datasets
+
+- Note: This is used on the premise that the public datasets are all included in the baseline collection. If users start having public assets in the future and update has to be made to the file to filter the baseline datasets.
+
+## Usage
+
+- This script in used through a backup bash script `backup.sh` on DX main repo
+- It backs up the data to the staging folder and the data is then extracted by the bash script
--- scripts/backup-baseline-datasets.py
+++ scripts/backup-baseline-datasets.py
@@ -1,4 +1,3 @@
-import json
 import logging
 import os
 import shutil
@@ -9,15 +8,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # MongoDB configuration
-MONGO_HOST = os.getenv('MONGO_HOST')
-MONGO_USERNAME = os.getenv('MONGO_USERNAME')
-MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
-MONGO_AUTH_SOURCE = os.getenv('MONGO_AUTH_SOURCE')
-DATABASE_NAME = 'the-data-explorer-db'
+MONGO_HOST = os.getenv("MONGO_HOST")
+MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_AUTH_SOURCE = os.getenv("MONGO_AUTH_SOURCE")
+DATABASE_NAME = "the-data-explorer-db"
 
 logger = logging.getLogger(__name__)
 load_dotenv()
-
 
 
 def setup_parsed_loc():
@@ -26,39 +24,50 @@ def setup_parsed_loc():
     """
     logger.debug("Setting up parsed location")
     try:
-        return os.environ['DATA_EXPLORER_SSR']
+        return os.environ["DATA_EXPLORER_SSR"]
     except Exception:
         return "./"
 
-DF_LOC=setup_parsed_loc()
+
+DF_LOC = setup_parsed_loc()
 
 
 def backup():
     """
-    Connect to the MongoDB and insert a dataset object.
+    Connect to MongoDB, get public datasets and backup their parsed json files.
     """
     try:
         client = pymongo.MongoClient(
             MONGO_HOST,
             username=MONGO_USERNAME,
             password=MONGO_PASSWORD,
-            authSource=MONGO_AUTH_SOURCE
+            authSource=MONGO_AUTH_SOURCE,
         )
         db = client[DATABASE_NAME]
 
         # Datasets
-        dataset_collection = db['Dataset']
+        dataset_collection = db["Dataset"]
         datasets = dataset_collection.find({"public": True})
 
         # Making sure the directory exists
-        os.makedirs(os.path.dirname("./staging/prepopulate-data/parsed-data-files/"), exist_ok=True)
-        os.makedirs(os.path.dirname("./staging/prepopulate-data/sample-data-files/"), exist_ok=True)
+        os.makedirs(
+            os.path.dirname("./staging/prepopulate-data/parsed-data-files/"),
+            exist_ok=True,
+        )
+        os.makedirs(
+            os.path.dirname("./staging/prepopulate-data/sample-data-files/"),
+            exist_ok=True,
+        )
 
         for dataset in datasets:
             parsed_df = f"{DF_LOC}parsed-data-files/{dataset['_id']}.json"
             sample_df = f"{DF_LOC}sample-data-files/{dataset['_id']}.json"
-            new_parsed_df = f"./staging/prepopulate-data/parsed-data-files/{dataset['_id']}.json"
-            new_sample_df = f"./staging/prepopulate-data/sample-data-files/{dataset['_id']}.json"
+            new_parsed_df = (
+                f"./staging/prepopulate-data/parsed-data-files/{dataset['_id']}.json"
+            )
+            new_sample_df = (
+                f"./staging/prepopulate-data/sample-data-files/{dataset['_id']}.json"
+            )
             # duplicate the parsed files if they exist
             if os.path.exists(parsed_df):
                 shutil.copy(parsed_df, new_parsed_df)
@@ -67,6 +76,7 @@ def backup():
     except Exception as e:
         print(str(e))
         return None
+
 
 if __name__ == "__main__":
     backup()
