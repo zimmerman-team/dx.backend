5ea81f7d9c73c5939ff78f85f9eb23f6d8b17f1b
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2024-08-26 11:28:54 +0200

    feat: updated date processing

--- services/preprocess_dataset.py
+++ services/preprocess_dataset.py
@@ -10,25 +10,186 @@ from services.read_data import read_data, read_data_from_api
 from services.ssr import create_ssr_parsed_file
 
 logger = logging.getLogger(__name__)
+
 DATE_FORMATS = [
-    "%Y-%m-%d",                  # YYYY-MM-DD
-    "%Y-%m-%d %H:%M:%S",         # YYYY-MM-DD HH:MM:SS
-    "%Y-%m-%d %H:%M:%S.%f",      # YYYY-MM-DD HH:MM:SS.microseconds
-    "%Y/%m/%d",                  # YYYY/MM/DD
-    "%Y/%m/%d %H:%M:%S",         # YYYY/MM/DD HH:MM:SS
-    "%Y/%m/%d %H:%M:%S.%f",      # YYYY/MM/DD HH:MM:SS.microseconds
-    "%m/%d/%Y",                  # MM/DD/YYYY
-    "%m/%d/%Y %H:%M:%S",         # MM/DD/YYYY HH:MM:SS
-    "%m/%d/%Y %H:%M:%S.%f",      # MM/DD/YYYY HH:MM:SS.microseconds
-    "%d/%m/%Y",                  # DD/MM/YYYY
-    "%d/%m/%Y %H:%M:%S",         # DD/MM/YYYY HH:MM:SS
-    "%d/%m/%Y %H:%M:%S.%f",      # DD/MM/YYYY HH:MM:SS.microseconds
-    # "%Y%m%d",                    # YYYYMMDD
-    # "%Y%m%d%H%M%S",              # YYYYMMDDHHMMSS
-    # "%Y%m%d%H%M%S%f",            # YYYYMMDDHHMMSS.microseconds
+    "%d %b %Y",  # d MMM yyyy
+    "%d %b %Y %H:%M:%S",  # d MMM yyyy HH:mm:ss
+    "%d %b %Y %H:%M:%S %z",  # d MMM yyyy HH:mm:ss Z
+    "%d %B %Y",  # d MMMM yyyy
+    "%d %B %Y %H:%M:%S %Z",  # d MMMM yyyy HH:mm:ss z
+    "%d-%m-%y",  # d-M-yy
+    "%d-%m-%y %H:%M",  # d-M-yy H:mm
+    "%d.%m.%y %H:%M",  # d.M.yy H:mm
+    "%d/%m/%Y",  # d/M/yyyy
+    "%d/%m/%Y %I:%M %p",  # d/M/yyyy h:mm a
+    "%d/%m/%Y %I:%M:%S %p",  # d/M/yyyy h:mm:ss a
+    "%d/%m/%Y %H:%M",  # d/M/yyyy HH:mm
+    "%d/%m/%Y %H:%M:%S",  # d/M/yyyy HH:mm:ss
+    "%d/%m/%y",  # d/MM/yy
+    "%d/%m/%y %H:%M",  # d/MM/yy H:mm
+    "%d/%b/%Y %H:%M:%S %z",  # d/MMM/yyyy H:mm:ss Z
+    "%d %B %Y",  # dd MMMM yyyy
+    "%d %B %Y %H:%M:%S %Z",  # dd MMMM yyyy HH:mm:ss z
+    "%d-%b-%y %I.%M.%S.%f %p",  # dd-MMM-yy hh.mm.ss.nnnnnnnnn a
+    "%d-%b-%Y",  # dd-MMM-yyyy
+    "%d-%b-%Y %H:%M:%S",  # dd-MMM-yyyy HH:mm:ss
+    "%d/%m/%y",  # dd/MM/yy
+    "%d/%m/%y %H:%M",  # dd/MM/yy HH:mm
+    "%d/%m/%y %H:%M:%S",  # dd/MM/yy HH:mm:ss
+    "%d/%m/%Y",  # dd/MM/yyyy
+    "%d/%m/%Y %I:%M %p",  # dd/MM/yyyy h:mm a
+    "%d/%m/%Y %I:%M:%S %p",  # dd/MM/yyyy h:mm:ss a
+    "%d/%m/%Y %H:%M",  # dd/MM/yyyy HH:mm
+    "%d/%m/%Y %H:%M:%S",  # dd/MM/yyyy HH:mm:ss
+    "%d/%b/%y %I:%M %p",  # dd/MMM/yy h:mm a
+    "%a %b %d %H:%M:%S %Z %Y",  # EEE MMM dd HH:mm:ss z yyyy
+    "%a, %d %b %Y %H:%M:%S %z",  # EEE, d MMM yyyy HH:mm:ss Z
+    "%A %d %B %Y",  # EEEE d MMMM yyyy
+    "%A %d %B %Y %H h %M %Z",  # EEEE d MMMM yyyy H' h 'mm z
+    "%A %d %B %Y %H h %M %Z",  # EEEE d MMMM yyyy HH' h 'mm z
+    "%A, %d %B %Y",  # EEEE, d MMMM yyyy
+    "%A, %d %B %Y %H:%M:%S o'clock %Z",  # EEEE, d MMMM yyyy HH:mm:ss 'o''clock' z
+    "%A, %B %d, %Y",  # EEEE, MMMM d, yyyy
+    "%A, %B %d, %Y %I:%M:%S %p %Z",  # EEEE, MMMM d, yyyy h:mm:ss a z
+    "%m-%d-%y",  # M-d-yy
+    "%m-%d-%y %I:%M %p",  # M-d-yy h:mm a
+    "%m-%d-%y %I:%M:%S %p",  # M-d-yy h:mm:ss a
+    "%m-%d-%y %H:%M",  # M-d-yy HH:mm
+    "%m-%d-%y %H:%M:%S",  # M-d-yy HH:mm:ss
+    "%m-%d-%Y",  # M-d-yyyy
+    "%m-%d-%Y %I:%M %p",  # M-d-yyyy h:mm a
+    "%m-%d-%Y %I:%M:%S %p",  # M-d-yyyy h:mm:ss a
+    "%m-%d-%Y %H:%M",  # M-d-yyyy HH:mm
+    "%m-%d-%Y %H:%M:%S",  # M-d-yyyy HH:mm:ss
+    "%m/%d/%y",  # M/d/yy
+    "%m/%d/%y %I:%M %p",  # M/d/yy h:mm a
+    "%m/%d/%y %H:%M:%S",  # M/d/yy H:mm:ss (HH:mm:ss, MM)
+    "%m/%d/%y %H:%M",  # M/d/yy HH:mm
+    "%m/%d/%Y",  # M/d/yyyy
+    "%m/%d/%Y %I:%M %p",  # M/d/yyyy h:mm a
+    "%m/%d/%Y %I:%M:%S %p",  # M/d/yyyy h:mm:ss a
+    "%m/%d/%Y %H:%M",  # M/d/yyyy HH:mm
+    "%m/%d/%Y %H:%M:%S",  # M/d/yyyy HH:mm:ss
+    "%m-%d-%y",  # MM-dd-yy
+    "%m-%d-%y %I:%M %p",  # MM-dd-yy h:mm a
+    "%m-%d-%y %I:%M:%S %p",  # MM-dd-yy h:mm:ss a
+    "%m-%d-%y %H:%M",  # MM-dd-yy HH:mm
+    "%m-%d-%y %H:%M:%S",  # MM-dd-yy HH:mm:ss
+    "%m-%d-%Y",  # MM-dd-yyyy
+    "%m-%d-%Y %I:%M %p",  # MM-dd-yyyy h:mm a
+    "%m-%d-%Y %I:%M:%S %p",  # MM-dd-yyyy h:mm:ss a
+    "%m-%d-%Y %H:%M",  # MM-dd-yyyy HH:mm
+    "%m-%d-%Y %H:%M:%S",  # MM-dd-yyyy HH:mm:ss
+    "%m/%d/%y",  # MM/dd/yy
+    "%m/%d/%y %I:%M %p",  # MM/dd/yy h:mm a
+    "%m/%d/%y %I:%M:%S %p",  # MM/dd/yy h:mm:ss a
+    "%m/%d/%y %H:%M",  # MM/dd/yy HH:mm
+    "%m/%d/%Y",  # MM/dd/yyyy
+    "%m/%d/%Y %I:%M %p",  # MM/dd/yyyy h:mm a
+    "%m/%d/%Y %I:%M:%S %p",  # MM/dd/yyyy h:mm:ss a
+    "%m/%d/%Y %H:%M",  # MM/dd/yyyy HH:mm
+    "%m/%d/%Y %H:%M:%S",  # MM/dd/yyyy HH:mm:ss
+    "%b %d %Y",  # MMM d yyyy
+    "%b %d, %Y",  # MMM d, yyyy
+    "%b %d, %Y %I:%M:%S %p",  # MMM d, yyyy h:mm:ss a
+    "%b.%d.%Y",  # MMM.dd.yyyy
+    "%B %d %Y",  # MMMM d yyyy
+    "%B %d, %Y",  # MMMM d, yyyy
+    "%B %d, %Y %I:%M:%S %p %Z",  # MMMM d, yyyy h:mm:ss z a
+    "%y-%m-%d",  # yy-MM-dd
+    "%Y-'W'%W-%w",  # YYYY-'W'w-c
+    "%Y-%j%z",  # yyyy-DDDXXX
+    "%Y-%m-%d %I:%M %p",  # yyyy-M-d h:mm a
+    "%Y-%m-%d %I:%M:%S %p",  # yyyy-M-d h:mm:ss a
+    "%Y-%m-%d %H:%M",  # yyyy-M-d HH:mm
+    "%Y-%m-%d %H:%M:%S",  # yyyy-M-d HH:mm:ss
+    "%Y-%m-%d",  # yyyy-MM-dd
+    "%Y-%m-%d %G",  # yyyy-MM-dd G
+    "%Y-%m-%d %I:%M %p",  # yyyy-MM-dd h:mm a
+    "%Y-%m-%d %I:%M:%S %p",  # yyyy-MM-dd h:mm:ss a
+    "%Y-%m-%d %H:%M:%S",  # yyyy-MM-dd HH:mm:ss
+    "%Y-%m-%d %H:%M:%S,%f",  # yyyy-MM-dd HH:mm:ss,SSS
+    "%Y-%m-%d %H:%M:%S,%f[%Z]",  # yyyy-MM-dd HH:mm:ss,SSS'['VV']'
+    "%Y-%m-%d %H:%M:%S,%fZ",  # yyyy-MM-dd HH:mm:ss,SSS'Z'
+    "%Y-%m-%d %H:%M:%S,%f%z",  # yyyy-MM-dd HH:mm:ss,SSSXXX
+    "%Y-%m-%d %H:%M:%S,%f%z[%Z]",  # yyyy-MM-dd HH:mm:ss,SSSXXX'['VV']'
+    "%Y-%m-%d %H:%M:%S.%f",  # yyyy-MM-dd HH:mm:ss.S
+    "%Y-%m-%d %H:%M:%S.%f",  # yyyy-MM-dd HH:mm:ss.SSS
+    "%Y-%m-%d %H:%M:%S.%f[%Z]",  # yyyy-MM-dd HH:mm:ss.SSS'['VV']'
+    "%Y-%m-%d %H:%M:%S.%fZ",  # yyyy-MM-dd HH:mm:ss.SSS'Z'
+    "%Y-%m-%d %H:%M:%S.%f%z",  # yyyy-MM-dd HH:mm:ss.SSSXXX
+    "%Y-%m-%d %H:%M:%S.%f%z[%Z]",  # yyyy-MM-dd HH:mm:ss.SSSXXX'['VV']'
+    "%Y-%m-%d %H:%M:%S%z[%Z]",  # yyyy-MM-dd HH:mm:ssXXX'['VV']'
+    "%Y-%m-%d %H:%M:%S%z",  # yyyy-MM-dd HH:mm:ssZ (ss'Z', ssX, ssXXX)
+    "%Y-%m-%dT%H:%M:%S",  # yyyy-MM-dd'T'HH:mm:ss
+    "%Y-%m-%dT%H:%M:%S,%f",  # yyyy-MM-dd'T'HH:mm:ss,SSS
+    "%Y-%m-%dT%H:%M:%S,%f[%Z]",  # yyyy-MM-dd'T'HH:mm:ss,SSS'['VV']'
+    "%Y-%m-%dT%H:%M:%S,%fZ",  # yyyy-MM-dd'T'HH:mm:ss,SSS'Z'
+    "%Y-%m-%dT%H:%M:%S,%f%z",  # yyyy-MM-dd'T'HH:mm:ss,SSSXXX
+    "%Y-%m-%dT%H:%M:%S,%f%z[%Z]",  # yyyy-MM-dd'T'HH:mm:ss,SSSXXX'['VV']'
+    "%Y-%m-%dT%H:%M:%S.%f",  # yyyy-MM-dd'T'HH:mm:ss.SSS
+    "%Y-%m-%dT%H:%M:%S.%f[%Z]",  # yyyy-MM-dd'T'HH:mm:ss.SSS'['VV']'
+    "%Y-%m-%dT%H:%M:%S.%fZ",  # yyyy-MM-dd'T'HH:mm:ss.SSS'Z'
+    "%Y-%m-%dT%H:%M:%S.%f%z",  # yyyy-MM-dd'T'HH:mm:ss.SSSXXX
+    "%Y-%m-%dT%H:%M:%S.%f%z[%Z]",  # yyyy-MM-dd'T'HH:mm:ss.SSSXXX'['VV']'
+    "%Y-%m-%dT%H:%M:%S%z[%Z]",  # yyyy-MM-dd'T'HH:mm:ssXXX'['VV']'
+    "%Y-%m-%dT%H:%M:%S%z",  # yyyy-MM-dd'T'HH:mm:ssZ (ss'Z', ssX, ssXXX)
+    "%Y-%m-%d%z",  # yyyy-MM-ddXXX
+    "%Y'W'%W%w",  # YYYY'W'wc
+    "%Y/%m/%d",  # yyyy/M/d
+    "%Y%m%d",  # yyyyMMdd
+    "%Y%m%d%z",  # yyyyMMddZ
+    "%Y-W%V",  # Week: 2024-W34
+    "%G-W%V-%u",  # Week with weekday: 2024-W34-5
+    "%Y-%j"  # Ordinal date: 2024-236
 ]
 
 
+def check_and_convert_dates(df):
+    """
+    Replacement function for dates. We want to be able to catch all dates.
+    Dates were sourced from
+      https://help.talend.com/en-us/data-preparation-user-guide/8.0/list-of-date-and-date-time-formats
+    And
+      https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+
+    :param df: dataframe to check
+    :return: dataframe with the dates converted to a standard format
+    """
+    for header in df.columns.tolist():
+        # get the first non na value in the column, alphabetically sorted in reverse (for american dates).
+        first_value = df[header].dropna().sort_values(ascending=False).iloc[0]
+
+        # get the presumed date format
+        presumed_dateformat = None
+        has_time = False
+        for date_format in DATE_FORMATS:
+            # check if my_date is in the date_format
+            try:
+                # value is not used, just checking for a ValueError
+                datetime.datetime.strptime(str(first_value), date_format)
+                presumed_dateformat = date_format
+                has_time = "H" in date_format or "I" in date_format
+                break
+            except ValueError:
+                continue
+        # Convert the column to a date if the presumed date format is found.
+        # Convert to YYYY-MM-DD if there is no time, otherwise convert to ISO8601
+        if presumed_dateformat is None:
+            continue
+        if has_time:
+            df[header] = df[header].apply(
+                lambda x, fmt=presumed_dateformat: datetime.datetime.strptime(str(x), fmt).isoformat())
+            # if all the time values end up as 00:00:00, remove the time
+            if df[header].apply(lambda x: x.endswith("00:00:00")).all():
+                df[header] = df[header].apply(lambda x: x[:10])
+        else:
+            df[header] = df[header].apply(
+                lambda x, fmt=presumed_dateformat: datetime.datetime.strptime(str(x), fmt).strftime("%Y-%m-%d"))
+    return df
+
+
+# DEPRECATED
 def is_datetime_column(column):
     """
     Check whether or not a column is majority datetime objects.
@@ -48,6 +209,7 @@ def is_datetime_column(column):
         return False
 
 
+# DEPRECATED
 def apply_date(string):
     """
     Convert a string to a datetime object, return a pandas df na if not possible
@@ -284,8 +446,11 @@ def preprocess_data_df(df):
     :return: dataframe with the data preprocessed
     """
     columns_with_strings_starting_with_numbers = []
+    df = check_and_convert_dates(df)
     for header in df.columns.tolist():
         # if there are 75% or more dates in the column, convert the column to a date
+        """
+        Deprecated date conversion 26/08/2024, remove after testing cycle
         has_datetime_values = is_datetime_column(df[header])
         if has_datetime_values:
             # Apply the date functions to a new column, replace the old column with the
@@ -294,7 +459,7 @@ def preprocess_data_df(df):
             df.drop(columns=[header], inplace=True)
             df.rename(columns={header + "_converted": header}, inplace=True)
             continue
-
+        """
         has_percentage_values = is_percentage_column(df[header])
         if has_percentage_values:
             df[header + ",%"] = df[header].apply(lambda x: convert_percentage_value(x))
